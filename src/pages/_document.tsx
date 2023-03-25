0ef23b95531a9d6a252109e90262e8ff0de47d34
salimia40 <puyaars@gmail.com>
2023-03-26 00:23:03 +0330

    init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { App, ConfigProvider } from 'antd';
+import fa from "antd/locale/fa_IR"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <ConfigProvider direction="rtl" locale={fa}>
+          <App>
+            <Main />
+            <NextScript />
+          </App>
+        </ConfigProvider>
       </body>
     </Html>
   )
